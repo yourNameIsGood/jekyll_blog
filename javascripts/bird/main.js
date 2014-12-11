6c6fcc75595718f90bda5f2cd80d613b3a1cd64b
YourNameIsGood <jack20039@gmail.com>
2014-12-11 13:59:28 +0800

    css

--- javascripts/bird/main.js
+++ javascripts/bird/main.js
@@ -1,7 +1,7 @@
 var xpos = 800;
 var ypos = 570;
-var birdimg = "{{site.baseurl}}/stylesheets/bird/bird.png";
-var pipeimg = "{{site.baseurl}}/stylesheets/bird/pipe.png";
+var birdimg = {{site.baseurl}}+"/stylesheets/bird/bird.png";
+var pipeimg = {{site.baseurl}}+"/stylesheets/bird/pipe.png";
 var game = new Phaser.Game(this.xpos, ypos, Phaser.AUTO, 'gameDiv');
 
 // Create our 'main' state that will contain the game
